ad9688ac2432348580566941150bddc01b48e1d3
gabiwritecode <t30330@gmail.com>
2024-01-04 02:38:44 +0800

    git commit -m "Update activity styles and rename image files"

--- js/activity.js
+++ js/activity.js
@@ -29,10 +29,10 @@ header.addEventListener('click',function(e){
   // 資料庫
   const data = [
     { url: '../image/activity/monet.jpg', title: '莫內:大師傑作', subtitle: '來自瑪摩丹美術館的鎮館之寶於馬德里限定展出，帶你一睹大師的風采' },
-    { url: '../image/activity/slider/candle concert.jpg', title: '馬德里燭光音樂會', subtitle: '燭光映照著美妙音樂，為您帶來令人難忘的音樂體驗' },
-    { url: '../image/activity/slider/ice rink.jpg', title: '西班牙廣場滑冰場', subtitle: '在西班牙廣場的冬季仙境中滑行，一同享受這個充滿節慶氛圍的滑冰體驗！' },
-    { url: '../image/activity/slider/pixer world.jpg', title: '皮克斯世界', subtitle: '奇幻動畫之旅，探索夢幻故事，盡情享受無限樂趣！' },
-    { url: '../image/activity/slider/reyes.jpg', title: '三王節遊行', subtitle: '"一月，準備好迎接三王節遊行的熱鬧氛圍！與家人共同迎接新年的繽紛樂趣。' },
+    { url: '../image/activity/candle concert.jpg', title: '馬德里燭光音樂會', subtitle: '燭光映照著美妙音樂，為您帶來令人難忘的音樂體驗' },
+    { url: '../image/activity/ice rink.jpg', title: '西班牙廣場滑冰場', subtitle: '在西班牙廣場的冬季仙境中滑行，一同享受這個充滿節慶氛圍的滑冰體驗！' },
+    { url: '../image/activity/pixer world.jpg', title: '皮克斯世界', subtitle: '奇幻動畫之旅，探索夢幻故事，盡情享受無限樂趣！' },
+    { url: '../image/activity/reyes.jpg', title: '三王節遊行', subtitle: '"一月，準備好迎接三王節遊行的熱鬧氛圍！與家人共同迎接新年的繽紛樂趣。' },
  
   ]
   // 獲取元素
